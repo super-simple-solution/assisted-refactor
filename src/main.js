8ec50fc8ad17fe1ab703766c6463bd6b20aa6272
Zhang Pan <zhangpanrobot@gmail.com>
2022-01-20 20:03:09 +0800

    support protobuf

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import '@/style.scss'
+
+import * as Icons from '@ant-design/icons-vue'
 
 const app = createApp(App)
+
+const icons = Icons
+for (const i in icons) {
+    // 循环注册组件
+    app.component(i, icons[i])
+}
 
 app.use(router)
 
--- src/views/proto.vue
+++ src/views/proto.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
+  <div class="proto-container">
     <a-row :gutter="10">
       <a-col :span="12" class="input-text">
-        <monaco-editor url="https://fe-modules.oss-cn-beijing.aliyuncs.com/monaco-editor-0.20.0/min" @editorDidMount="editorDidMount" :options="data.editOption" :style="data.editStyle" class="editor" ref="source" v-model="data.text" theme="vs-dark" language="proto" />
-        <a-button danger icon="a-icon-delete" class="bt-op" @click="clear">清空</a-button>
-        <a-button type="primary" icon="a-icon-document" class="bt-op" @click="format">格式化</a-button>
+        <monaco-editor url="https://fe-modules.oss-cn-beijing.aliyuncs.com/monaco-editor-0.20.0/min" @editorDidMount="editorDidMount" :options="data.editOption" :style="data.editStyle" class="editor" ref="source" v-model="data.text" theme="vs-dark" language="protobuf" />
+        <div class="m10">
+          <a-button danger class="mr10" @click="clear">
+            <template #icon>
+              <clear-outlined />
+            </template>
+            清空
+          </a-button>
+          <a-button type="primary" @click="format">
+            <template #icon>
+              <format-painter-outlined />
+            </template>
+            格式化
+          </a-button>
+        </div>
       </a-col>
       <a-col span="12" class="input-text">
-        <monaco-editor url="https://fe-modules.oss-cn-beijing.aliyuncs.com/monaco-editor-0.20.0/min" :options="data.editOption" :style="data.editStyle" class="editor" v-model="data.result" theme="vs-dark" language="js" />
+        <monaco-editor url="https://fe-modules.oss-cn-beijing.aliyuncs.com/monaco-editor-0.20.0/min" :options="data.editOption" :style="data.editStyle" class="editor" v-model="data.result" theme="vs-dark" language="javascript" />
       </a-col>
     </a-row>
   </div>
@@ -15,7 +27,9 @@
 
 <script setup>
 import MonacoEditor from 'vue-monaco-cdn'
+import registerProtobuf from 'monaco-proto-lint'
 import { ref, reactive } from "vue"
+
 let data = reactive({
   loading: true,
   text: '',
@@ -31,6 +45,7 @@ let source = ref(null)
 let editor = null
 function editorDidMount() {
   editor = source.value.getMonaco()
+  registerProtobuf(monaco)
   data.loading = false
 }
 function format() {
@@ -43,5 +58,4 @@ function clear() {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
